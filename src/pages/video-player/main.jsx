979c5f7370045287c05a935351d1434a96d9803a
Arpit <arpitkumar01923@gmail.com>
2022-04-23 15:26:25 +0530

    added image to video player

--- src/pages/video-player/main.jsx
+++ src/pages/video-player/main.jsx
@@ -31,7 +31,7 @@ export const Main = () => {
             <span className="sub-container1">
               <img
                 className="round sm"
-                src="https://media.istockphoto.com/photos/businessman-silhouette-as-avatar-or-default-profile-picture-picture-id476085198?b=1&k=20&m=476085198&s=170667a&w=0&h=Ct4e1kIOdCOrEgvsQg4A1qeuQv944pPFORUQcaGw4oI="
+                src={filterVideo.image}
                 alt="avatar-image"
               />
               <span className="margin-top-0_5 left-sub-container">
